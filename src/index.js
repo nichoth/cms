98bcd3d519dff3b2a3b69b7643e0f982d9704f7e
nichoth <nichoth@gmail.com>
2020-07-10 17:55:28 -0700

    works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+var React = require('react')
+// var h = require('react').createElement
+var ReactDOM = require('react-dom')
+
 function Example (props) {
     return <div>example</div>
 }
+
+ReactDOM.render(<Example />, document.getElementById('content'))
 
